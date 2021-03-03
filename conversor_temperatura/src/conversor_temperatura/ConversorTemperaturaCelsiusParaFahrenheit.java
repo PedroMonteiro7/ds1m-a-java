d06db43cf0b306f0237975ccd2e81731c43b7cf4
PEDRO DOS SANTOS MONTEIRO SILVA <21192977@CFP127ALUNO.LOCAL>
2021-03-03 11:21:15 -0300

    Aula 03-03-2021 - Turma A

---
+++ conversor_temperatura/src/conversor_temperatura/ConversorTemperaturaCelsiusParaFahrenheit.java
@@ -0,0 +1,16 @@
+package conversor_temperatura;
+
+public class ConversorTemperaturaCelsiusParaFahrenheit {
+	
+	public static void main(String[] args) {
+		
+		float celsius = 27.0f;
+		float fahrenheint = (9 * celsius + 160) / 5;
+		
+		System.out.print(fahrenheint + " " + "graus fahrenheint");
+
+		
+	}
+	
+
+}
